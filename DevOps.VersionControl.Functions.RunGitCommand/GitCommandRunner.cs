e4f31188c77b4681e29f1c52a71911e09316f23c
Christopher Dorst <chris@dorst.email>
2017-12-27 16:30:40 -0600

    Adding GitCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitCommand/GitCommandRunner.cs
@@ -0,0 +1,15 @@
+﻿using static Common.Functions.JoinCommandLineArguments.CommandLineArgumentsJoiner;
+using static Common.Functions.RunCommandLine.CommandLineRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitCommand
+{
+    public static class GitCommandRunner
+    {
+        private const string git = nameof(git);
+
+        public static void Git(string directory, string command, params string[] arguments)
+            => Run(directory,
+                command: git,
+                arguments: Join(command, arguments));
+    }
+}
